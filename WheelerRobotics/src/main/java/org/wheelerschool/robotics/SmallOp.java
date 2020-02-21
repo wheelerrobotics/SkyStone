ed7cabc10aae4057210aae4879bbebd29a462714
Lucien Gaitskell <lucieng@gaitskell.com>
2020-02-21 15:50:36 -0500

    Add drive motor definitions

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/SmallOp.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/SmallOp.java
@@ -1,15 +1,22 @@
 package org.wheelerschool.robotics;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 // OpMode class
 public class SmallOp extends OpMode {
 
+    // DEFINE motor objects:
+    DcMotor driveLeft;
+    DcMotor driveRight;
+
     // RUNS ON Initialization
     @Override
     public void init() {
-
+        // SET UP motor objects:
+        driveLeft = hardwareMap.dcMotor.get("driveLeft"); // use names defined in application
+        driveRight = hardwareMap.dcMotor.get("driveRight");
     }
 
     // RUNS CONTINUOUSLY (after start)
