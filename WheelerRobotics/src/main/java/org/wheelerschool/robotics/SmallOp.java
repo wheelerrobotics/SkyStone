c1894f8f4f67efc607b57e44af95a57a6dea00f4
Lucien Gaitskell <lucieng@gaitskell.com>
2020-02-21 15:46:27 -0500

    Add framework opmode

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/SmallOp.java
@@ -0,0 +1,20 @@
+package org.wheelerschool.robotics;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+// OpMode class
+public class SmallOp extends OpMode {
+
+    // RUNS ON Initialization
+    @Override
+    public void init() {
+
+    }
+
+    // RUNS CONTINUOUSLY (after start)
+    @Override
+    public void loop() {
+
+    }
+}
